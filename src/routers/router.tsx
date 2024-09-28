97ed7375042ae96fbd757c0af123c72e0f1e3cd0
yerin <dev.yerin@gmail.com>
2024-09-28 19:20:54 +0900

    feat: funnel 경로 추가

--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Home } from '../pages/Home';
+import { Funnel } from '../pages/Funnel';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/funnel',
+    element: <Funnel />,
   },
 ]);
 
