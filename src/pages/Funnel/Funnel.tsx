ad54833a41b565c2a6ef15ce667a9f3cf123f809
yerin <dev.yerin@gmail.com>
2024-09-28 19:43:12 +0900

    style: funnel 페이지 배경 컬러 정의

--- src/pages/Funnel/Funnel.tsx
+++ src/pages/Funnel/Funnel.tsx
@@ -1,5 +1,14 @@
+import { Input } from '../../components/ui/input';
+
 const Funnel = () => {
-  return <div>Funnel</div>;
+  return (
+    <div className="bg-[#f9f9f9]">
+      <div className="max-w-md mx-auto h-screen bg-white p-4 py-10">
+        <strong className="text-2xl">지금 휴대폰 번호를 그대로 쓸까요?</strong>
+        <Input type="email" placeholder="Email" />
+      </div>
+    </div>
+  );
 };
 
 export default Funnel;
